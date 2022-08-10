313dc0c8561eba440ef373b04cac3e8336dc0b32
rb183918 <anastasiya.golova@rosbank.ru>
2022-08-10 17:25:51 +0300

    Add method og greeting user

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,11 @@
 package hexlet.code;
 
+import static hexlet.code.Cli.greetingUser;
+
 public class App {
 
     public static void main(String[] args) {
         System.out.println("Welcome to the Brain Games!");
+        greetingUser();
     }
 }
---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,14 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Cli {
+
+    public static void greetingUser() {
+        System.out.println("May I have your name?");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.println("Hello, " + name + "!");
+    }
+
+}
