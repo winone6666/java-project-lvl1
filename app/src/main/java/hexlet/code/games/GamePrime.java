46a4117f20d5f7e336c479d4a81808624c36a4c2
rb183918 <anastasiya.golova@rosbank.ru>
2022-08-18 12:41:34 +0300

    Add realization Prime game

--- app/src/main/java/hexlet/code/games/GamePrime.java
+++ app/src/main/java/hexlet/code/games/GamePrime.java
@@ -1,5 +1,7 @@
 package hexlet.code.games;
 
+import static hexlet.code.Engine.*;
+
 public class GamePrime {
 
     public static void printPrimeGameDescription() {
@@ -7,6 +9,24 @@ public class GamePrime {
     }
 
     public static void playPrimeGame(){
+        int temp;
+        boolean isPrime = true;
+        for (int i = 0; i < COUNT_OF_QUESTION; i++) {
 
+            int randNum = (int) (Math.random() * MULTIPLIER_TO_GET_INT);
+            String question = Integer.toString(randNum);
+            questionsAndAnswers[0][i] = question;
+
+            for (int j = 2; j<=randNum/2; j++) {
+                temp = randNum % j;
+                if (temp == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            referenceAnswer[i] = isPrime? "yes" : "no";
+
+        }
+        compareUserAnswerAndReference(questionsAndAnswers, referenceAnswer);
     }
-}
+    }
