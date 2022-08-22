0fc1397f3274cf4c8c2a740196d4902a598ed7e4
rb183918 <anastasiya.golova@rosbank.ru>
2022-08-22 21:34:54 +0300

    Check repo

---
+++ src/main/java/hexlet/code/App.java
@@ -0,0 +1,68 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+import static hexlet.code.Cli.greetingUser;
+import static hexlet.code.Engine.printGreeting;
+import static hexlet.code.games.GameCalculator.playCalculatorgame;
+import static hexlet.code.games.GameEven.playEvenGame;
+import static hexlet.code.games.GameGCD.playGCDGame;
+import static hexlet.code.games.GamePrime.playPrimeGame;
+import static hexlet.code.games.GameProgression.playProgressionGame;
+
+public class App {
+    //check repo
+    private static final int EXIT = 0;
+    private static final int GREET = 1;
+    private static final int EVEN = 2;
+    private static final int CALC = 3;
+    private static final int GCD = 4;
+    private static final int PROGRESSION = 5;
+    private static final int PRIME = 6;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("""
+                Please enter the game number and press Enter.
+                1 - Greet
+                2 - Even
+                3 - Calculator
+                4 - GCD
+                5 - Progression
+                6 - Prime
+                0 - Exit
+                """);
+
+        int numOfGame = scanner.nextInt();
+        System.out.println("Your choice: " + numOfGame);
+
+        switch (numOfGame) {
+            case GREET -> greetingUser();
+            case EVEN -> {
+                printGreeting();
+                playEvenGame();
+            }
+            case CALC -> {
+                printGreeting();
+                playCalculatorgame();
+            }
+            case GCD -> {
+                printGreeting();
+                playGCDGame();
+            }
+            case PROGRESSION -> {
+                printGreeting();
+                playProgressionGame();
+            }
+            case PRIME -> {
+                printGreeting();
+                playPrimeGame();
+            }
+            case EXIT -> {
+                System.out.println("Your choice: " + numOfGame + "." + " Bye!");
+            }
+            default -> System.out.println("Unexpected number: " + numOfGame + ". "
+                        + "Please select game again.");
+        }
+    }
+}
